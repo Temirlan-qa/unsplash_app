3a6c01cab2234361e520db45cba9783214d5040d
Temirlan-qa <temirlanaazbek@gmail.com>
2023-02-11 02:14:04 +0600

    finish

--- lib/logic/data/services_api_provider.dart
+++ lib/logic/data/services_api_provider.dart
@@ -5,6 +5,7 @@ class GalleryListProvider{
   Future<List<Photo>> fetchPhotos() async {
     final response = await http.get(Uri.parse(
         'https://api.unsplash.com/photos/?client_id=ab3411e4ac868c2646c0ed488dfd919ef612b04c264f3374c97fff98ed253dc9'));
+        // ab3411e4ac868c2646c0ed488dfd919ef612b04c264f3374c97fff98ed253dc9 = $id future можно сделать так
     if (response.statusCode == 200) {
       List<dynamic> photosJson = json.decode(response.body);
       return photosJson.map((photoJson) => Photo.fromJson(photoJson)).toList();
